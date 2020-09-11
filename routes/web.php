90924fba385ab8e5074ab95a3a1c96f6d6863f36
marijoo <marijan@kolossal.io>
2020-09-11 19:26:41 +0200

    Demo route:cache bug

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [
+            'localeSessionRedirect'
+        ],
+    ], function () {
+
+        Route::view('/', 'welcome');
+
+    }
+);
